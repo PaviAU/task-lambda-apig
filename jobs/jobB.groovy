a4ca72959bcefc1f972bce88ee62206db8062973
PaviAU <155062620+PaviAU@users.noreply.github.com>
2023-12-28 17:00:31 +0530

    Create jobB.groovy

---
+++ jobs/jobB.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        stage('build') {
+            steps {
+                echo "Job B"
+            }
+        }
+        stage('triggerjobA') {
+            steps {
+               // build job: "jobA", wait: true
+            }
+        }
+    }
+}
