e1c324f0c6d7bf36e2681aae5ecd397dc31b22d6
PaviAU <155062620+PaviAU@users.noreply.github.com>
2023-12-29 14:47:16 +0530

    created terrafile dir

---
+++ terrafiles/README.md
@@ -0,0 +1,1 @@
+# task-lambda-apig
---
+++ terrafiles/index.js
@@ -0,0 +1,20 @@
+exports.handler = async (event) => {
+    // Your Lambda function logic here
+    const firstName = event.queryStringParameters?.first_name || 'John';
+    const lastName = event.queryStringParameters?.last_name || 'Doe';
+    
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ first_name: firstName, last_name: lastName }),
+        body: JSON.stringify({
+            message: "Hello from Lambda!"
+            // Add more data as needed
+
+        })
+    };
+
+    return response;
+};
