68cb8359b0ddd4c8bbdd2f4c3a126926061a8959
PaviAU <pavidevops072022@gmail.com>
2023-12-29 14:54:28 +0530

    moved to terrafile dir

--- README.md
+++
@@ -1,1 +0,0 @@
-# task-lambda-apig
--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
- /*  backend "s3" {
-
-    profile        = "default"
-    encrypt        = "true"
-    dynamodb_table = "task-lambda-apig-table"
-  } */
-}
