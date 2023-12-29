6205f80c3a9ae28016ea3cf595b874f2f3f53c20
PaviAU <pavidevops072022@gmail.com>
2023-12-29 15:55:14 +0530

    uncommented backend file

--- jobs/jobB.groovy
+++ jobs/jobB.groovy
@@ -2,10 +2,10 @@ pipeline {
     agent any
 
     environment{
-       /*  def s3BucketName = "task-lambda-apigateway-bucket-29122023";
+        def s3BucketName = "task-lambda-apigateway-bucket-29122023";
         def s3BucketRegion = "us-east-1";
         def tfStateFile = "./terraform.tfstate";
-        def dynamoDBTable = "task-lambda-apigateway-table-29122023"; */
+        def dynamoDBTable = "task-lambda-apigateway-table-29122023"; 
         def gitInfraBranchName          = "main";
     }
     stages {
@@ -14,20 +14,17 @@ pipeline {
                 echo "Job B"
             }
         }
-       /*  stage('Checkout stage') {
-            steps {
-                    git branch: 'main', credentialsId: '7302c566-e079-4a9d-af26-b6eb72aff582', url: 'https://github.com/PaviAU/task-lambda-apig'
-                
-            } */
-
-             stage('Git Checkout infra'){
+          stage('Git Checkout infra'){
             steps{
                 //Clone the git repository.
                 //checkout scmGit(branches: [[name: "*/main"]], extensions: [], userRemoteConfigs: [[credentialsId: 'github-ssh-key', url: 'git@github.com:PaviAU/task-lambda-apig.git']])
                 checkout scmGit(branches: [[name: "*/${gitInfraBranchName}"]], extensions: [], userRemoteConfigs: [[credentialsId: '7302c566-e079-4a9d-af26-b6eb72aff582', url: 'https://github.com/PaviAU/task-lambda-apig']])
                 sh script: "git checkout ${gitInfraBranchName}"
                 sh script: "git pull origin ${gitInfraBranchName}"
+                sh script: "cd terrafiles"
                 sh script: "pwd && ls"
+                
+
             }
         }
 
@@ -36,24 +33,24 @@ pipeline {
             stage('Terraform init & Plan stage') {
             steps {
                     dir('terrafiles/'){
-                        // sh "terraform init -force-copy -reconfigure -backend-config  'bucket=${s3BucketName}' -backend-config  region=${s3BucketRegion} -backend-config  'key=${tfstateFile}' -backend-config dynamodb_table='${dynamoDBTable}'"
-                        sh "terraform init"
-                        // sh "terraform fmt -list=true -write=false -diff=true -check=true"
+                        sh "terraform init -force-copy -reconfigure -backend-config  'bucket=${s3BucketName}' -backend-config  region=${s3BucketRegion} -backend-config  'key=${tfstateFile}' -backend-config dynamodb_table='${dynamoDBTable}'"
+                        //sh "terraform init"
+                        sh "terraform fmt -list=true -write=false -diff=true -check=true"
                         sh "terraform validate"
-                    // sh "terraform plan"
+                        sh "terraform plan"
                   }
             }
                 
             } 
 
-            stage('Terraform plan stage') {
+            /* stage('Terraform plan stage') {
             steps {
                     dir('terrafiles/'){
                         sh "terraform plan"
                     }
                   }
                 
-            }  
+            }   */
 
            /*  stage('Terraform apply stage') {
             steps {
