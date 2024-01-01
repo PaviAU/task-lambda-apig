fe9544b6f57c7b48f8dceff16a7f621d1f1faf39
PaviAU <pavidevops072022@gmail.com>
2024-01-01 19:57:44 +0530

    deleted backend

--- terrafiles/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-   backend "s3" {
-    bucket = "lambda-apigateway-bucket29122023"
-    key = "terrafiles/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = "true"
-    dynamodb_table = "task-lambda-apig-table"
-  }  
-}
